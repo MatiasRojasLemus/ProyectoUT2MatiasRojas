95612b453677299ae91667daa3fa9fe22d6ca8c7
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2024-10-25 01:02:56 +0200

    Código del movimiento horizontal ya completado.

--- Proyecto2DMatiasRojas/Assets/Scripts/PlayerController.cs
+++ Proyecto2DMatiasRojas/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private SpriteRenderer sprtRnd;
     public Rigidbody2D rb;
     public float speedMove;
     public float jumpingPower;
+    public SpriteRenderer sprtRnd;
+
+
     private float horizontal;
-    
+    private bool isFacingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +21,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        checkMovement();
+    }
+
+    public void checkMovement(){
         rb.velocity = new Vector2(horizontal * speedMove, rb.velocity.y);
+        
+        if(!isFacingRight && horizontal > 0f){
+             //Girar a la derecha
+            isFacingRight = true;
+            sprtRnd.flipX = false;
+        }
+        else if(isFacingRight && horizontal < 0f){
+            //Girar a la izquierda
+            isFacingRight = false;
+            sprtRnd.flipX = true;
+        }
     }
 
     public void Move(InputAction.CallbackContext context){
-        horizontal = context.ReadValue<Vector2>().x; 
+        horizontal = context.ReadValue<Vector2>().x;
+        Debug.Log(horizontal); 
     }
 }
