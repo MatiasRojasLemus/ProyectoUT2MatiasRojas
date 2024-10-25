f88e759ec395a68f370ed932e890d4b1d1e5af1f
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2024-10-25 02:13:39 +0200

    Transición del estado Idle a estado Run implementado correctamente

--- Proyecto2DMatiasRojas/Assets/Scripts/PlayerController.cs
+++ Proyecto2DMatiasRojas/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,33 +8,37 @@ public class PlayerController : MonoBehaviour
     public float speedMove;
     public float jumpingPower;
     public SpriteRenderer sprtRnd;
-
-
+    public Animator anim;
     private float horizontal;
     private bool isFacingRight = true;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         checkMovement();
     }
 
     public void checkMovement(){
+        if(Mathf.Abs(horizontal) == 0f){
+            //Si no se mueve
+            anim.SetBool("isRunning",false);
+        }
+        else{
+            //Si se mueve
+            anim.SetBool("isRunning",true);
+        }
+
         rb.velocity = new Vector2(horizontal * speedMove, rb.velocity.y);
         
         if(!isFacingRight && horizontal > 0f){
-             //Girar a la derecha
             isFacingRight = true;
             sprtRnd.flipX = false;
         }
         else if(isFacingRight && horizontal < 0f){
-            //Girar a la izquierda
             isFacingRight = false;
             sprtRnd.flipX = true;
         }
@@ -43,4 +48,5 @@ public class PlayerController : MonoBehaviour
         horizontal = context.ReadValue<Vector2>().x;
         Debug.Log(horizontal); 
     }
+
 }
