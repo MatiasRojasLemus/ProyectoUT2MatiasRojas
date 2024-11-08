0f3ab9ab008f8dd7d2cd20c61f6d08be7a2f881e
MatiasRojasLemus <149407312+MatiasRojasLemus@users.noreply.github.com>
2024-11-08 21:02:20 +0100

    Reajuste Camara y construcción ligera del nivel con el objeto suelo.

--- Proyecto2DMatiasRojas/Assets/Scripts/GroundCheck.cs
+++ Proyecto2DMatiasRojas/Assets/Scripts/GroundCheck.cs
@@ -1,26 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundCheck : MonoBehaviour
 {
     public static bool isGrounded;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        isGrounded = true;
+        if (collision.gameObject.tag == "Suelo")
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        
     }
 
     private void OnTriggerExit2D(Collider2D collision)
